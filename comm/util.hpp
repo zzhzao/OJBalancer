a298fb8b98dd62f293054df81e514e29e11315be
赵泽浩 <3516422381@qq.com>
2026-03-16 18:22:25 +0800

    编译运行模块的完善

--- comm/util.hpp
+++ comm/util.hpp
@@ -1,88 +1,124 @@
 #pragma once
 
-#include<iostream>
-#include<string>
-#include<sys/types.h>
-#include<sys/stat.h>
-#include<unistd.h>
-#include<sys/time.h>
-const std::string temp_path = "./temp/"; 
+#include <iostream>
+#include <string>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <atomic>
+#include<fstream>
+const std::string temp_path = "./temp/";
 
 namespace ns_util
 {
     class PathUtil
     {
-        public:
-        
-        
-            static std::string AddSuffix(const std::string &file_name,const std::string &suffix)   
-            {
-                std::string path_name = temp_path;
-                path_name += file_name;
-                path_name += suffix;
-                return path_name;
-            }
-            // 传入的为文件名，没有任何后缀
-            // 编译时 使用的文件
-            static std::string Src(const std::string &file_name)
-            {
-                return AddSuffix(file_name,".cpp");
-            }
-            static std::string Exe(const std::string &file_name)
-            {
-                return AddSuffix(file_name,".exe");
-            }
+    public:
+        static std::string AddSuffix(const std::string &file_name, const std::string &suffix)
+        {
+            std::string path_name = temp_path;
+            path_name += file_name;
+            path_name += suffix;
+            return path_name;
+        }
+        // 传入的为文件名，没有任何后缀
+        // 编译时 使用的文件
+        static std::string Src(const std::string &file_name)
+        {
+            return AddSuffix(file_name, ".cpp");
+        }
+        static std::string Exe(const std::string &file_name)
+        {
+            return AddSuffix(file_name, ".exe");
+        }
 
-            static std::string CompilerError(const std::string &file_name)
-            {
-                return AddSuffix(file_name,".compile_error");
-            }
-            //  运行时 使用的文件
-            static std::string Stdin(const std::string &file_name)
-            {
-                return AddSuffix(file_name,".stdin");
-            }
-            static std::string Stdout(const std::string &file_name)
-            {
-                return AddSuffix(file_name,".stdout");
-
-            }
-            static std::string Stderr(const std::string &file_name)
-            {
-                return AddSuffix(file_name,".stderr");
-            }
-            
-    };
-
-    class FileUtil
-    { 
-        public:
-           static bool IsFileExists(const std::string &path_name)
-           {
-              struct stat st;
-              if(stat(path_name.c_str(),&st) == 0)
-              {
-                  return true;
-              }
-              return false;
-           }
-           static std::string  UniqFileName()
-           {
-
-           }
-           static bool WriteFile(const std::string& target,const std::string &code)
-           {
-              
-           }
+        static std::string CompilerError(const std::string &file_name)
+        {
+            return AddSuffix(file_name, ".compile_error");
+        }
+        //  运行时 使用的文件
+        static std::string Stdin(const std::string &file_name)
+        {
+            return AddSuffix(file_name, ".stdin");
+        }
+        static std::string Stdout(const std::string &file_name)
+        {
+            return AddSuffix(file_name, ".stdout");
+        }
+        static std::string Stderr(const std::string &file_name)
+        {
+            return AddSuffix(file_name, ".stderr");
+        }
     };
     class TimeUtil
     {
-        public:
-            static std::string GetTimeStamp()
-            {
-                struct timeval _time;
-                gettimeofday(&_time,NULL);
-                return std::to_string(_time.tv_sec);
-            }
+    public:
+        static std::string GetTimeStamp()
+        {
+            struct timeval _time;
+            gettimeofday(&_time, NULL);
+            return std::to_string(_time.tv_sec);
+        }
+        static std::string GetTimeMs()
+        {
+            struct timeval _time;
+            gettimeofday(&_time, NULL);
+            return std::to_string(_time.tv_sec * 1000 + _time.tv_usec / 1000);
+        }
     };
+    class FileUtil
+    {
+    public:
+        static bool IsFileExists(const std::string &path_name)
+        {
+            struct stat st;
+            if (stat(path_name.c_str(), &st) == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        static std::string UniqFileName()
+        {
+            // 利用时间戳 和 原子性机制来生成
+            static std::atomic_uint id(0);
+            id++;
+            std::string ms = TimeUtil::GetTimeMs();
+            std::string uniq_id = std::to_string(id);
+            return ms + "_" + uniq_id;
+
+        }
+        static bool WriteFile(const std::string &target, const std::string &code)
+        {
+            std::ofstream out(target);
+            if(!out.is_open())
+            {
+                return false;
+            }
+            out.write(code.c_str(), code.size());
+            out.close();
+        }
+        // keep 来判断是否保留 \n
+        static bool ReadFile(const std::string &target,std::string *content,bool keep = false)
+        {
+            (*content).clear();
+            std::ifstream in(target);
+            if(!in.is_open())
+            {
+                return false;
+            }
+            std::string line;
+            // getline 不保存行分割符,有时候需要保留\n
+            // getline 重载了强制类型转化
+            while(std::getline(in,line))
+            {
+                (*content) += line;
+                (*content) += keep ? "\n" : "";
+            }
+            in.close();
+            return true;
+        }
+    };
+
 };
--- compile_server/compile_run.hpp
+++ compile_server/compile_run.hpp
@@ -14,7 +14,47 @@ namespace ns_compile_and_run
     {
 
     public:
-        static void Start(const std::string &in_json, const std::string *out_json)
+        // code > 0 : 收到信号导致异常崩溃
+        // code < 0 ：整个过程非运行报错  代码为空，编译报错等
+
+        static std::string CodeToDesc(int code, const std::string &file_name)
+        {
+            std::string desc;
+            std::string _stderr;
+            switch (code)
+            {
+            // 待完善
+            case 0:
+                desc = "编译运行成功";
+                break;
+            case -1:
+                desc = "用户提交的代码为空";
+                break;
+            case -2:
+                desc = "未知错误";
+                break;
+            case -3:
+
+                FileUtil::ReadFile(PathUtil::CompilerError(file_name), &_stderr, true);
+                desc = _stderr;
+                break;
+            case SIGABRT:
+                desc = "内存超过范围";
+                break;
+            case SIGXCPU:
+                desc = "CPU运行超时";
+                break;
+            case SIGFPE:
+                desc = "浮点数溢出";
+                break;
+
+            default:
+                desc = "未知：" + std::to_string(code);
+                break;
+            }
+            return desc;
+        }
+        static void Start(const std::string &in_json, std::string *out_json)
         {
             //  输入 ： code  input
             // code : 用户提交的代码   input ： 用户提交的代码对应输入
@@ -30,14 +70,75 @@ namespace ns_compile_and_run
             std::string input = in_value["input"].asString();
             int cpu_limit = in_value["cpu_limit"].asInt();
             int mem_limit = in_value["mem_limit"].asInt();
+
+            Json::Value out_value;
+            std::string file_name;
+            int status_code = 0;
+            int run_result = 0;
             if (code.size() == 0)
             {
+                // out_value["status"] = -1;
+                // out_value["reason"] = "用户代码是空的";
+                // return;
+                status_code = -1;
+                goto END;
             }
-            std::string file_name = FileUtil::UniqFileName();
 
-            FileUtil::WriteFile(PathUtil::Src(file_name),code);  // 形成临时src文件
-            Compiler::Compile(file_name);
-            Runner::Run(file_name,cpu_limit,mem_limit);
+            // 毫秒级时间戳 + 原子性递增 ： 来保证唯一值
+
+            // 形成临时src文件
+            file_name = FileUtil::UniqFileName();
+            if (!FileUtil::WriteFile(PathUtil::Src(file_name), code))
+            {
+                // out_value["status"] = -2; // 写入文件失败
+                // out_value["reason"] = "发生了未知错误";
+                // return;
+                status_code = -2;
+                goto END;
+            }
+            if (!Compiler::Compile(file_name))
+            {
+                // out_value["status"] = -3; // 编译失败
+                // out_value["reason"] = FileUtil::ReadFile(PathUtil::CompilerError(file_name));
+                // return;
+                status_code = -3;
+                goto END;
+            }
+            run_result = Runner::Run(file_name, cpu_limit, mem_limit);
+            if (run_result < 0)
+            {
+                // out_value["status"] = -2; // 内部错误
+                // out_value["reason"] = "发生了未知错误";
+                // return;
+                status_code = -2;
+                goto END;
+            }
+            else if (run_result > 0)
+            {
+                // code代表信号
+                //  out_value["status"] = code;
+                //  out_value["reason"] = SignoToDesc(run_result);//将信号错误转化为原因
+                status_code = run_result;
+            }
+            else
+            {
+                // 运行成功
+                status_code = 0;
+            }
+        END:
+            out_value["status"] = status_code;
+            out_value["reason"] = CodeToDesc(status_code, file_name);
+            if (status_code == 0)
+            {
+                std::string _stdout;
+                FileUtil::ReadFile(PathUtil::Stdout(file_name), &_stdout, true);
+                out_value["stdout"] = _stdout;
+                std::string _stderr;
+                FileUtil::ReadFile(PathUtil::Stderr(file_name), &_stderr, true);
+                out_value["stderr"] = _stderr;
+            }
+            Json::StyledWriter writer;
+            *out_json = writer.write(out_value);
         }
     };
 }
