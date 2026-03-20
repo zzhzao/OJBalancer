a734d87af70e212ecb5d170bed8cfbcadaa75b90
赵泽浩 <3516422381@qq.com>
2026-03-20 17:36:16 +0800

    负载均衡的整体结构

--- oj_server/oj_control.hpp
+++ oj_server/oj_control.hpp
@@ -6,6 +6,7 @@
 #include"../comm/log.hpp"
 #include<vector>
 #include"oj_view.hpp"
+#include<mutex>
 namespace ns_control
 {
     using namespace std;
@@ -13,10 +14,67 @@ namespace ns_control
     using namespace ns_log;
     using namespace ns_model;
     using namespace ns_view;
+    // 提供服务的主机
+    class Machine
+    {
+    public:
+        std::string ip;
+        int port;
+        uint64_t load;  // 用于计数
+        std::mutex *mtx;
+    public:
+        Machine():ip(""),port(0),load(0),mtx(nullptr)
+        {}
+        ~Machine(){}
+    };
+
+    const std::string service_machine = "./conf/service_machine.conf";
+
+    // 负载均衡
+    class LoadBlance
+    {
+    private:
+    // 可以给我们提供编译服务的所有主机
+    // 每一台主机都有自己的下标，来充当当前主机的id
+        std::vector<Machine> machines;  
+        std::vector<int> online;  // 所有在线的主机id
+        std::vector<int> offline;  //  所有离线的主机id
+
+    public:
+        LoadBlance()
+        {
+            assert(LoadConf(service_machine));
+        }
+        ~LoadBlance()
+        {}
+        bool LoadConf(const std::string &machine_list)
+        {
+
+        }
+        bool SmartChoice()
+        {
+
+        }
+        void OfflineMachine()
+        {
+
+        }
+        void OnlineMachine()
+        {
+            
+        }
+    };
+
+
+
+
+
+///////////////////////////////////////////////
+
+
+    //核心业务
     class Control
     {
-
-
     private:
         Model model_;  // 提供后台数据
         View view_;   // 提供网页渲染
@@ -59,6 +117,17 @@ namespace ns_control
             }
             return ret;
         }
-     
+        void Judge(const std::string in_json,std::string *out_json)
+        {
+            // in_json   id   code   input
+            // 1.对in_json反序列化  得到id 代码 input
+
+            // 2. 拼接用户代码和测试用例代码
+
+            // 3. 选择负载最低的主机，
+            // 4. 然后发起http请求，得到结果
+
+            // 5. 将结果赋值给out_json
+        }
     };
 }
